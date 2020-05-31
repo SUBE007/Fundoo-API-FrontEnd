f74ddac5910007d6b56e10d1f047c06b575a1a30
SUBE007 <chaurasiyasube007@gmail.com>
2020-05-31 13:15:11 +0530

    [SUBE007]Refactored  modified the Login.css and Login.css page and added forgotpassword.css style file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './App.css';
-import Registration from "./component/registration";
-import Login from "./component/login";
-import ForgotPassword from "./component/forgetPassword";
-import {BrowserRouter as Router,Switch,Route} from "react-router-dom";
- 
+import Router from './Router/Router';
 
-class App extends Component {
-  render() {
+export default class App extends React.Component {
+  render () {
     return (
-      <Router>
-         <Switch>  
-          <Route path="/register" component={Registration}></Route>
-          <Route path="/login" component={Login}></Route>
-          <Route path="/forgetpassword" component={ForgotPassword} />
-          <Route path="/" exact component={Login} />
-         </Switch>
-      </Router>
+      <div>
+        <Router />
+      </div>
     );
   }
 }
-
- 
-export default App;
--- src/constants/userApiConstants.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-     register:"/user/register",
-     login: "/user/login"
- }; 
