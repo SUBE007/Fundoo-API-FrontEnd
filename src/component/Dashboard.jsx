906e6ac64a8ea1359d82c95e7062bb8bf16f20ee
SUBE007 <chaurasiyasube007@gmail.com>
2020-06-02 16:23:40 +0530

    [SUBE007]Added Dashboard.css file in CSS and modified  Dashboard.jsx

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,16 +1,71 @@
-
 import React ,{Component}from "react";
+import {AppBar,IconButton,Tooltip, createMuiTheme,Typography, }
+from "@material-ui/core";
+import MenuIcon from "@material-ui/icons/Menu";
+import keep from "../assets/KeepLogo.png";
+import "../CSS/Dashboard.css";
 
-class Dashboard extends  Component{
+const theme = createMuiTheme({
+    overrides: {
+      MuiPopover: {
+        paper: { width: "30%",},
+       },
+    },
+  });
+    
  
-    render() {
+class Dashboard extends  Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+          headerName: "",
+        };
+      }
+    
+      render() {
         return (
-             <div >
-               <h1>Hello SUBE, Dashboard world!!!</h1>
-             </div>
-        )
-    }
+          <div>
+            <AppBar id="appBar_decor">
+              <AppBar className="appBar_flex">
+                <div className="menu_andname">
+                  <Tooltip title="Main Menu" arrow>
+                    <IconButton  class="reduce_padding" id="butone"
+                        onClick={this.handleClick}
+                    >
+                    <MenuIcon id="reduceButtonSize" />
+                    </IconButton>
+                  </Tooltip>
+                  <div className="keepImageSpace">
+                        {this.state.headerName === "" ? (
+                        <React.Fragment>
+                            <img className="keep_img" src={keep} alt="F" />
+                            <Typography  id="fundoo" variant="h5" 
+                            style={{marginLeft: "1%",}}
+                             >
+                            <div className="fundoo1">
+                                <span style={{ color: "Blue" }}>F</span>
+                                <span style={{ color: "Red" }}>U</span>
+                                <span style={{ color: "Yellow" }}>N</span>
+                                <span style={{ color: "Blue" }}>D</span>
+                                <span style={{ color: "Green" }}>O</span>
+                                <span style={{ color: "Red" }}>O</span> 
+                            </div>
+                            </Typography>
+                        </React.Fragment>
+                        ) 
+                        : (
+                        <Typography id="fundoo" variant="h5" color="textSecondary">
+                            {this.state.headerName}
+                        </Typography>
+                        )}
+                  </div>
+                </div>
+              </AppBar>
 
+            </AppBar>
+          </div>
+        );
+    }
 }
 
 export default Dashboard;
